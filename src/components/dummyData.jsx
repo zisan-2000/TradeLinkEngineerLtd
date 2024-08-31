23ac4e3566507e5a81cf47210c9e2c41c91d566f
MD. ZULFIKER HOSSAIN <zisan19393871@gmail.com>
2024-08-31 18:07:59 +0600

    homepage color changed

--- src/components/dummyData.jsx
+++ src/components/dummyData.jsx
@@ -1,9 +1,9 @@
 // dummy.jsx
 const themeColors = {
-  homepage: "bg-slate-600", // Added theme color for homepage
+  homepage: "bg-red-500", // Added theme color for homepage
   about: "bg-blue-500",
   clients: "bg-green-500",
-  contacts: "bg-red-500",
+  contacts: "bg-slate-500",
   maintenance: "bg-orange-500",
   media: "bg-purple-500",
   partners: "bg-teal-500",
